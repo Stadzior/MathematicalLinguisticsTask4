dcd0bcc961bf62f76f9a18c2a60156f774a9d884
Stadzior <Stadzior@gmail.com>
2017-06-12 10:46:22 +0200

    yep

--- MathematicalLinguisticsTask4/MathematicalLinguisticsTask4/ReversePolishNotationConverter.cs
+++ MathematicalLinguisticsTask4/MathematicalLinguisticsTask4/ReversePolishNotationConverter.cs
@@ -14,18 +14,24 @@ namespace MathematicalLinguisticsTask4
         private Operator LeftBracket = new Operator() { Symbol = '(' };
         private Operator RightBracket = new Operator() { Symbol = ')' };
 
-        private Queue<char> OutputQueue = new Queue<char>();
+        private Queue<string> OutputQueue = new Queue<string>();
         private Stack<Operator> OperatorsStack = new Stack<Operator>();
 
         public string Convert(string infixNotationInput)
         {
-            foreach (char symbol in infixNotationInput)
+            for (int i = 0; i < infixNotationInput.Length; i++)
             {
+                var symbol = infixNotationInput[i];
+
                 if (IsUnrecognized(symbol))
                     throw new ArgumentException($"Unrecognized symbol \"{symbol}\".");
 
+                if (i>0 && IsDigit(infixNotationInput[i-1]))
+
+
                 if (IsDigit(symbol))
-                    OutputQueue.Enqueue(symbol);
+                    if(IsDigit)
+                    OutputQueue.Enqueue(symbol.ToString());
                 else if (IsOperator(symbol))
                 {
                     var o1 = GetOperatorFromSymbol(symbol);
@@ -34,7 +40,7 @@ namespace MathematicalLinguisticsTask4
                            (o1.IsRightAssociative && o1.PriorityLevel < OperatorsStack.Peek().PriorityLevel)))
                     {
                         var poppedOperator = OperatorsStack.Pop();
-                        OutputQueue.Enqueue(poppedOperator.Symbol);
+                        OutputQueue.Enqueue(poppedOperator.Symbol.ToString());
                     }
                     OperatorsStack.Push(o1);
                 }
@@ -48,7 +54,7 @@ namespace MathematicalLinguisticsTask4
                     while(!OperatorsStack.Peek().Equals(LeftBracket))
                     {
                         var poppedOperator = OperatorsStack.Pop();
-                        OutputQueue.Enqueue(poppedOperator.Symbol);
+                        OutputQueue.Enqueue(poppedOperator.Symbol.ToString());
 
                         if (!OperatorsStack.Any())
                             throw new ArgumentException("Missing left bracket.");
@@ -60,12 +66,14 @@ namespace MathematicalLinguisticsTask4
             while(OperatorsStack.Any())
             {
                 var poppedOperator = OperatorsStack.Pop();
-                OutputQueue.Enqueue(poppedOperator.Symbol);
+                OutputQueue.Enqueue(poppedOperator.Symbol.ToString());
             }
 
             StringBuilder resultBuilder = new StringBuilder();
             while (OutputQueue.Any())
+            {
                 resultBuilder.Append(OutputQueue.Dequeue());
+            }
 
             return resultBuilder.ToString();
         }
